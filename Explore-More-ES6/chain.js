6751ad992c14584c20d85935cb9b20285d81195a
RaselDev28 <rasel.dev28@gmail.com>
2025-06-09 00:04:07 +0600

    data

--- Explore-More-ES6/chain.js
+++ Explore-More-ES6/chain.js
@@ -2,4 +2,13 @@
 
 const data = [{name : 'Raahim' , Age: 21, address: 'Bangladesh'}];
 
-console.log(data[0].address);
+// console.log(data[0].address);
+
+const products={
+    count: 50000,
+    data:[
+        {id: 1, name: 'Dell Laptop', Price: 100000},
+        {id: 1, name: 'HP Laptop', Price: 110000},
+    ]
+}
+console.log(products.data[1].Price);
