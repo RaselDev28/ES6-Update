deb379d71563cfa4ada6acee07a9e4e629637d5a
RaselDev28 <rasel.dev28@gmail.com>
2025-06-27 23:45:23 +0600

    filter

---
+++ Explore-More-ES6/filter.js
@@ -0,0 +1,11 @@
+const numbers = [12, 12, 11, 10, 4, 2, 43];
+
+const selected = numbers.filter(p => p % 2 === 1);
+
+console.log(selected);
+
+const names = ['Rasel', 'Rahim', 'karim', 'sujon'];
+
+const evenName = names.filter(name => name.length % 2 === 0);
+
+console.log(evenName);
