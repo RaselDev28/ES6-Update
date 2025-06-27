3272631627d0b9ebeed81b36ccc87c8771f5662f
RaselDev28 <rasel.dev28@gmail.com>
2025-06-27 23:53:27 +0600

    find

---
+++ Explore-More-ES6/find.js
@@ -0,0 +1,5 @@
+const numbers = [12, 12, 11, 10, 4, 2, 43];
+
+// const selected = numbers.find(number => number > 15);
+const selected = numbers.find(number => number > 11);
+console.log(selected);
