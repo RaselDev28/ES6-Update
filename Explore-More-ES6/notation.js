9aefd2044c332b098419dc2bbeb6bdb6ac24646e
RaselDev28 <rasel.dev28@gmail.com>
2025-07-02 00:06:36 +0600

    notation

---
+++ Explore-More-ES6/notation.js
@@ -0,0 +1,10 @@
+const person={
+    name: 'Rasel',
+    job: 'Web-Developer'
+}
+// Dot Notatiion
+console.log(person.job);
+// Bracket Notation
+console.log(person['job'])
+const prop='job';
+console.log(person[prop])
