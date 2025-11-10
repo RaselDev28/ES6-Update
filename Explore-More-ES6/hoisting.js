b0e4c1d16171c1bf75cc919688196a46163a7f22
RaselDev28 <rasel.dev28@gmail.com>
2025-11-10 22:53:40 +0600

    hoisting

---
+++ Explore-More-ES6/hoisting.js
@@ -0,0 +1,13 @@
+function A(){
+    x=6;
+    console.log(x);
+    var x;
+
+    
+    // Hoisting is run but can not be set undifined value into the memory
+    // let x; 
+    // const x ;
+
+}
+
+A();
--- Explore-More-ES6/notation.js
+++ Explore-More-ES6/notation.js
@@ -8,3 +8,7 @@ console.log(person.job);
 console.log(person['job'])
 const prop='job';
 console.log(person[prop])
+
+
+// function min(nums) { return Math.min(nums) }
+// console.log(min( [1,3,2 ]));
