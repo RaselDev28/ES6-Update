6861f91d93d277c318cc1d07ab48aff4da09555f
RaselDev28 <rasel.dev28@gmail.com>
2025-11-10 22:57:32 +0600

    hoist

--- Explore-More-ES6/hoisting.js
+++ Explore-More-ES6/hoisting.js
@@ -3,10 +3,12 @@ function A(){
     console.log(x);
     var x;
 
+
     
-    // Hoisting is run but can not be set undifined value into the memory
-    // let x; 
-    // const x ;
+    // let x; // Hoisting is run but can not be set undifined value into the memory
+
+
+    // const x ; // Before hoisting code given syntax error because const value you cannot be assign
 
 }
 
