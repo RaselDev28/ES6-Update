24f0e5511cca098022b6a2d5b88b0f90a1d12ec8
RaselDev28 <rasel.dev28@gmail.com>
2025-11-15 23:07:55 +0600

    spread

--- Explore-More-ES6/spread.js
+++ Explore-More-ES6/spread.js
@@ -3,3 +3,8 @@ const friends=["Hena", "Moina", "Sokina"];
 const newArray=['Hero Alom', 'Lia Moni', ...friends]; //Spread Operator
 
 console.log(newArray);
+
+
+const numbers=[1,2,3,5,4,3,23,55,32,34];
+
+console.log(Math.max(...numbers)); //Math.max with sperad operator
