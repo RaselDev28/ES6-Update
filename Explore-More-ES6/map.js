4e50839b89461d55bced6756f2c395e89d545c9a
RaselDev28 <rasel.dev28@gmail.com>
2025-11-22 23:24:44 +0600

    map

--- Explore-More-ES6/map.js
+++ Explore-More-ES6/map.js
@@ -1,29 +1,35 @@
 const numbers = [2, 4, 5, 43, 33];
 // Using map==> map will be loop through in an array and callback the function and also return the result
 
-// Using normal function
+// // Using normal function
 
-function double(num) {
-    return num * 2;
-}
+// function double(num) {
+//     return num * 2;
+// }
 
-// Using arrow function
+// // Using arrow function
 
-const double2 = n => n*2;
+// const double2 = n => n*2;
 
-const output = numbers.map(double2);
-console.log(output);
+// const output = numbers.map(double2);
+// console.log(output);
 
-const result = numbers.map(double);
-// console.log(result);
+// const result = numbers.map(double);
+// // console.log(result);
 
-// Useing normal loop through
+// // Useing normal loop through
 
-const doubled=[];
+// const doubled=[];
 
-for(const num of numbers){
-    const double=num*2;
-    doubled.push(double);
-}
+// for(const num of numbers){
+//     const double=num*2;
+//     doubled.push(double);
+// }
 
-// console.log(doubled);
+// // console.log(doubled);
+
+const newNumber=numbers.map(value=>{
+    return value*value;
+    
+})
+console.log(newNumber);
