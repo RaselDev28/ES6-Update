0041eea231a75ba5b0b438cd3e9a54d8a14ae2e1
RaselDev28 <rasel.dev28@gmail.com>
2025-11-24 23:53:46 +0600

    foreach

--- Explore-More-ES6/foreach.js
+++ Explore-More-ES6/foreach.js
@@ -1,7 +1,20 @@
-const numbers =[12, 12,11,10,4,2,43]
+// const numbers =[12, 12,11,10,4,2,43]
 
-// const result=numbers.forEach(n=> console.log(n));
-const result=numbers.forEach(n=> {
+// // const result=numbers.forEach(n=> console.log(n));
+// const result=numbers.forEach(n=> {
 
-});
-console.log(result);
+// });
+// console.log(result);
+
+const products=[
+    {id:1, name:'Iphone', price:1200, color:'red'},
+    {id:2, name:'Samsung', price:1300, color:'white'},
+    {id:3, name:'vivo', price:1400, color:'black'},
+    {id:4, name:'Oppo', price:1500, color:'gold'}
+];
+
+products.forEach(product=>{
+    if(product.price>1300){
+        console.log(product.price);
+    }
+})
