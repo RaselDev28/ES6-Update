b5e534a9ec21ec8367d62a249f9896b066794477
RaselDev28 <rasel.dev28@gmail.com>
2025-12-01 23:55:42 +0600

    this

--- Explore-More-ES6/this.js
+++ Explore-More-ES6/this.js
@@ -1,13 +1,21 @@
-class Person{
-    constructor(name, age){
-        this.name=name;
-        this.age=age;
-    }
-sleep(){
-    console.log(`sleeping now: ${this.name}`)
-}
-}
+// class Person{
+//     constructor(name, age){
+//         this.name=name;
+//         this.age=age;
+//     }
+// sleep(){
+//     console.log(`sleeping now: ${this.name}`)
+// }
+// }
 
-const Rasel = new Person('Rasel', 24);
-console.log(Rasel);
-Rasel.sleep();
+// const Rasel = new Person('Rasel', 24);
+// console.log(Rasel);
+// Rasel.sleep();
+ class car{
+    constructor(name,price){
+        this.name=name;
+        this.price=price;
+    }
+ }
+ const car1= new car('BMW', 15000000);
+ console.log(car1);
