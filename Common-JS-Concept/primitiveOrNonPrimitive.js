0319da09f02405506f422fc0684283d06a4b00b4
RaselDev28 <rasel.dev28@gmail.com>
2025-12-25 00:07:11 +0600

    dataType

---
+++ Common-JS-Concept/primitiveOrNonPrimitive.js
@@ -0,0 +1,16 @@
+//Primitve Data type
+/*
+let person ="Rasel";
+let newPerson = person;
+ person="Mahim"; 
+
+ console.log(newPerson); //Cannot be change or Immutable
+ */
+
+ // Non Premitive Data Type
+
+ let person ={name:"Rasel"};
+let newPerson = person;
+ person.name="Mahim"; 
+
+ console.log(newPerson); //Mutuable or can be change the property or value
