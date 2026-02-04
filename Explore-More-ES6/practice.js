ee71f95f3b33dd31eea4fbb3977488c39808201f
RaselDev28 <rasel.dev28@gmail.com>
2026-02-04 21:03:11 +0600

    practice

---
+++ Explore-More-ES6/practice.js
@@ -0,0 +1,28 @@
+// Task-1..................
+/*
+const nums = [1, 2, 3, 4];
+
+const newNumber=nums.map(value=>{
+    return value+value;
+})
+console.log(newNumber);
+
+
+// Task-2....................
+const nums = [10, 15, 20, 25, 30];
+const evenNum = nums.filter(value => {
+    return value % 2 === 0;
+})
+console.log(evenNum)
+
+// Task-3.......................
+const nums = [20, 40, 55, 60, 10];
+const newNumber=nums.find(value=>{
+    return value>50;
+})
+console.log(newNumber)
+*/
+
+// Task-4.................
+const nums = [1, 2, 3];
+nums.forEach(num=>{console.log(num*5)})
